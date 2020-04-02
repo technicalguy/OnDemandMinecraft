933e94e1a1112d8106a21b837219a14a6b697edb
Alex Mueller <2556429+lemonpkt@users.noreply.github.com>
2020-04-03 08:40:30 +1100

    Add more detailed logs

--- server.py
+++ server.py
@@ -7,9 +7,13 @@ import time
 import paramiko
 import os
 import io
+import warnings
 
 app = Flask(__name__)
 CORS(app)
+
+# Stop paramiko from clogging the log output with depreciation warnings
+warnings.filterwarnings(action='ignore', module='.*paramiko.*')
 
 # Paraminko ssh information
 key_string = os.getenv('SSH_KEY').replace('\\n', '\n')
@@ -20,12 +24,13 @@ sshClient.set_missing_host_key_policy(paramiko.AutoAddPolicy())
 # Waits for the server to reach a valid state so that commands can be executed on the server
 
 
-def serverWaitOk(instanceIp, client):
-
+def serverWaitOk(client, instanceIp, instanceId):
     checksPassed = False
     status = 'initializing'
-    instanceIds = [os.getenv('INSTANCE_ID')]
+    instanceIds = [instanceId]
 
+    print('Waiting for instance', instanceId,
+          'to have status ok', flush=True)
     while (not checksPassed) and (status == 'initializing'):
         statusCheckResponse = client.describe_instance_status(
             InstanceIds=instanceIds)
@@ -34,12 +39,13 @@ def serverWaitOk(instanceIp, client):
         instanceStatus = instanceStatus['InstanceStatus']
         status = instanceStatus['Status']
         checksPassed = status == 'ok'
+        print('Instance', instanceId, 'status is', status, flush=True)
         time.sleep(5)
 
     if checksPassed:
         initServerCommands(instanceIp)
     else:
-        print('An error has occurred booting the server')
+        print('An error has occurred booting the server', flush=True)
 
 # SSH connects to server and executes command to boot minecraft server
 
@@ -52,19 +58,19 @@ def initServerCommands(instanceIp):
 
         _, stdout, _ = sshClient.exec_command("screen -ls | grep minecraft")
         if stdout.read():
-            print('Screen already exists. Will not run minecraft again.')
+            print('Screen already exists. Will not run minecraft again.', flush=True)
             return
 
         sshClient.exec_command(
             "screen -dmS minecraft bash -c 'sudo java " + os.getenv('MEMORY_ALLOCATION', '') + "-jar server.jar nogui'")
-        print('Starting minecraft')
+        print('Starting minecraft', flush=True)
 
         # close the client connection once the job is done
         sshClient.close()
 
     except Exception as err:
         print('Error running server commands:')
-        print(err)
+        print(err, flush=True)
 
 # Main endpoint for loading the webpage
 
@@ -77,11 +83,12 @@ def loadIndex():
 @app.route('/initServerMC', methods=['POST'])
 def initServerMC():
     inputPass = request.form['pass']
-    returnData = {}
 
     message = "Password Incorrect!"
 
     if inputPass == os.getenv('SERVER_PASSWORD'):
+        print('IP', request.remote_addr,
+              'has supplied correct password', flush=True)
         # Instantiate server here or return ip address if already running
         client = boto3.client(
             'ec2',
@@ -90,8 +97,10 @@ def initServerMC():
             region_name=os.getenv('EC2_REGION')
         )
         message = manageServer(client)
+    else:
+        print('IP', request.remote_addr,
+              'gave wrong password \'{}\''.format(inputPass), flush=True)
 
-    print(message)
     return render_template('index.html', ipMessage=message)
 
 
@@ -106,42 +115,41 @@ def manageServer(client):
 
     instances = reservation['Instances']
 
-    print("\nSERVER INSTANCES\n")
-    print(instances)
-    print("\n")
     if len(instances) > 0:
         instance = instances[0]
+        print('Found instance with id', instance['InstanceId'], flush=True)
         state = instance['State']
         stateName = state['Name']
 
         if (stateName == 'stopped') or (stateName == 'shutting-down'):
-            # SETUP MULTIPROCESSING HERE INSTEAD OF REDIS
-            returnString = startServer(client)
+            returnString = startServer(client, instance['InstanceId'])
         elif stateName == 'running':
             # Restart minecraft if needed
             initServerCommands(instance['PublicIpAddress'])
             returnString = 'IP: ' + instance['PublicIpAddress']
         else:
-            returnString = 'ERROR'
+            print('Instance state \'{}\' is unrecognized'.format(
+                stateName), flush=True)
+            returnString = 'Server is in an unrecognized state, please try again in a few minutes'
     return returnString
 
 # Starts the specified AWS Instance from the configuration
 
 
-def startServer(client):
+def startServer(client, instanceId):
     # Gets proper variables to attempt to instantiate EC2 instance and start minecraft server
     returnString = 'ERROR'
-    instanceIds = [os.getenv('INSTANCE_ID')]
+    instanceIds = [instanceId]
     response = client.start_instances(InstanceIds=instanceIds)
+    print('AWS EC2 START RESPONSE\n')
+    print(response)
+    print('\n', flush=True)
 
     stateCode = 0
 
     while not (stateCode == 16):
+        print('Waiting for instance', instanceId, 'to start', flush=True)
         time.sleep(3)
-
-        print('\nAWS EC2 START RESPONSE\n')
-        print(str(response))
-        print('\n')
 
         response = client.describe_instances(InstanceIds=instanceIds)
         reservations = response['Reservations']
@@ -153,14 +161,10 @@ def startServer(client):
         state = instance['State']
         stateCode = state['Code']
 
-        print("\nSERVER INSTANCES\n")
-        print(instances)
-        print("\n")
-
     ipAddress = instance['PublicIpAddress']
     returnString = 'Server is starting, this may take a few minutes.\nIP: ' + ipAddress
     # SETUP MULTIPROCESSING HERE INSTEAD OF REDIS
-    p = Process(target=serverWaitOk, args=(ipAddress, client))
+    p = Process(target=serverWaitOk, args=(client, ipAddress, instanceId))
     p.start()
     return returnString
 
