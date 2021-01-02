0bddcda4fc2d0570d4bfd34e09a6a227387b8dc6
S. C. Steenkamp <theguywholikeslinux@gmail.com>
2021-01-02 23:15:21 +0000

    all of the changes and stuff

--- configuration.py
+++ configuration.py
@@ -1,7 +1,7 @@
 class Config:
     # AWS Information
-    ec2_region = ""  # Same as environment variable EC2_REGION
-    ec2_amis = ['']
-    ec2_keypair = ''
-    ec2_secgroups = ['']
-    ec2_instancetype = ''
+    ec2_region = "eu-west-2" # London  # Same as environment variable EC2_REGION
+    ec2_amis = ['ami-09c4a4b013e66b291']
+    ec2_keypair = 'OnDemandMinecraft'
+    ec2_secgroups = ['sg-0441198b7b0617d3a']
+    ec2_instancetype = 't3.small'
--- server.py
+++ server.py
@@ -59,7 +59,7 @@ def initServerCommands(instanceIp):
 
         # Execute a command(cmd) after connecting/ssh to an instance
         stdin, stdout, stderr = sshClient.exec_command(
-            "screen -dmS minecraft bash -c 'sudo java " + os.getenv('MEMORY_ALLOCATION', '') + "-jar server.jar nogui'")
+            "screen -dmS minecraft bash -c 'sudo java -Djava.net.preferIPv4Stack=true " + os.getenv('MEMORY_ALLOCATION', '') + "-jar server.jar nogui'")
         print('Starting minecraft', flush=True)
         # close the client connection once the job is done
         sshClient.close()
